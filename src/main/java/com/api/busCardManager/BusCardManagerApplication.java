e1a7d0dd38dd7fe36dd5bb64013c2f015ddab326
Vini5577 <ugopessoa@gmail.com>
2024-07-17 19:39:15 -0300

    init project

---
+++ src/main/java/com/api/busCardManager/BusCardManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.busCardManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusCardManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusCardManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/busCardManager/BusCardManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.busCardManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusCardManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
